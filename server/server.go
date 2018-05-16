f002ff6566c7582d4dc06c325bd15829e78c8828
ramyakmehra <ramyakmehra10@gmail.com>
2018-05-17 01:13:56 +0530

    Update server.go

--- server/server.go
+++ server/server.go
@@ -16,8 +16,8 @@ import (
 	"time"
 
 	"github.com/NYTimes/gziphandler"
-	"github.com/jpillora/cloud-torrent/engine"
-	"github.com/jpillora/cloud-torrent/static"
+	"github.com/ramyakmehra/cloud-torrent/engine"
+	"github.com/ramyakmehra/cloud-torrent/static"
 	"github.com/jpillora/cookieauth"
 	"github.com/jpillora/requestlog"
 	"github.com/jpillora/scraper/scraper"
